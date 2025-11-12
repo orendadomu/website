6a79b6ea1d25566f8a7003b1719fb64b0c8e55f6
Victoria Trachuk <victoriatrachuk23@gmail.com>
2025-11-12 19:34:31 +0200

    Check env

--- src/composables/useFetch.js
+++ src/composables/useFetch.js
@@ -2,6 +2,7 @@
 import { createFetch } from '@vueuse/core'
 
 const API = import.meta.env.VITE_BASE_URL
+console.log('env', import.meta.env)
 
 export const useMyFetch = createFetch({
   baseUrl: API,
